fa8783a97d96dcf99725e1acbb6554f383fc2276
amar <amar@gmail.com>
2022-06-30 19:59:16 +0530

    again

---
+++ finallydo/src/test/java/finallydo/yes.java
@@ -0,0 +1,31 @@
+package finallydo;
+
+import java.util.Scanner;
+
+public class yes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int cash = sc.nextInt();
+		
+		if (cash < 10) {
+			System.out.println("you want more cash");
+		}
+		
+			else if (cash > 10 && cash < 50) {
+			 System.out.println("you get only pen");
+			}
+		
+			 else {
+				 System.out.println("You get both");
+			 }
+			 }
+		
+
+             
+
+	}
+
+
